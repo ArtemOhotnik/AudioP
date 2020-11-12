bb40bde55130a23d5aff4c862b697230588ed0ee
ArtemOhotnik <artemohotnik2006@gmail.com>
2020-11-12 08:53:56 +0000

    123

---
+++ build/precache-manifest.a006b7cad4f18faca5819f6a2d7d8520.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7f66d79bc220df6e7beb9968efa6f22",
+    "url": "/index.html"
+  },
+  {
+    "revision": "976cac7261675e0feecb",
+    "url": "/static/css/main.7f0fc96f.chunk.css"
+  },
+  {
+    "revision": "9a95d3e9d25ce757b482",
+    "url": "/static/js/2.b3cc2e74.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.b3cc2e74.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "976cac7261675e0feecb",
+    "url": "/static/js/main.b39003dd.chunk.js"
+  },
+  {
+    "revision": "8b1c6a44a5cfcd22430f",
+    "url": "/static/js/runtime-main.b7d213a2.js"
+  },
+  {
+    "revision": "fcddaf58983a2ff6a3129751b5f9d7bb",
+    "url": "/static/media/1.fcddaf58.mp3"
+  },
+  {
+    "revision": "017def15b34d37ca8a858f27b971f039",
+    "url": "/static/media/2.017def15.mp3"
+  },
+  {
+    "revision": "735acb02f772a25c188acc363b989551",
+    "url": "/static/media/3.735acb02.mp3"
+  },
+  {
+    "revision": "6f932147b31b16aa67a57d27b44152a6",
+    "url": "/static/media/4.6f932147.mp3"
+  },
+  {
+    "revision": "d9818e2cd572ff6ff6e716c09ae803a7",
+    "url": "/static/media/5.d9818e2c.mp3"
+  },
+  {
+    "revision": "887ed4a0a8e9ee5e5751d0a2c726d8bb",
+    "url": "/static/media/6.887ed4a0.mp3"
+  },
+  {
+    "revision": "d6d5e26df381d6c6af02fe5bcf9c590c",
+    "url": "/static/media/7.d6d5e26d.mp3"
+  }
+]);
